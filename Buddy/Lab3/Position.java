3ab9314f9b46db362d8abd1aa5b3594d9eb10497
MaanKalaria <142599275+MaanKalaria@users.noreply.github.com>
2024-06-25 10:11:35 +0530

    Add files via upload

---
+++ Buddy/Lab3/Position.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Position{
+    public static void main(String[] args){
+        int smallno = 0;
+        int bigno = 0;
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[5];
+        System.out.print("enter array: ");
+        for(int i = 0; i <5; i++) {
+            a[i] = sc.nextInt();
+        }
+    }
+}
