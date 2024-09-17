aeebe63e5ced424f11cd2c77de6df7c46f0287e9
MaanKalaria <142599275+MaanKalaria@users.noreply.github.com>
2024-09-17 11:02:02 +0530

    Add files via upload

---
+++ Buddy/lab 17/Hashing.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Hashing {
+
+    static int[] hash = new int[20];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[15];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random()*100000);
+        }
+        System.out.println(Arrays.toString(arr));
+        Store(arr);
+        System.out.println(Arrays.toString(hash));
+    }
+
+    public static void Store(int arr[]) {
+        int index;
+        for (int i = 0; i < arr.length; i++) {
+            index = hash(arr[i]);
+            hash[index] = arr[i];
+        }
+    }
+
+    public static int hash(int n) {
+        int index = (n % 18) + 2;
+        if (hash[index] != 0) {
+            while (hash[index] != 0) {
+                index = (++index) % 20;
+            }
+        }
+        return index;
+    }
+}
